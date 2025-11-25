ab593785e3383a72d4ea72e279ea4316ebd2cd78
liyinan <liyinan@liyinans-Mac-mini.local>
2025-11-25 16:11:03 +0800

    Proxy bug fix.

--- install-macos.sh
+++ install-macos.sh
@@ -8,6 +8,8 @@ cat >> "$HOME/.zshrc" << EOF
 
 ##################### Quick command start ####################
 PATH=$HOME/code/shell/quick-command/command/macos/:\$PATH
+alias proxy="source proxy"
+alias unproxy="source unproxy"
 alias lsort="ls -lSX"
 alias ltime="ls -ltr"
 ####################  Quick command end  #####################
